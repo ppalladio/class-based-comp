320189950971d9825b6ef98552283a186a94562d
ppalladio <nine.slide@gmail.com>
2022-10-31 22:10:17 +0100

    conver users.js to class based comp

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,37 +1,44 @@
-import { useState } from 'react';
+import { useState, Component } from 'react';
 import User from './User';
 
 import classes from './Users.module.css';
 
 const DUMMY_USERS = [
-  { id: 'u1', name: 'Max' },
-  { id: 'u2', name: 'Manuel' },
-  { id: 'u3', name: 'Julie' },
+    { id: 'u1', name: 'Max' },
+    { id: 'u2', name: 'Manuel' },
+    { id: 'u3', name: 'Julie' },
 ];
 
-const Users = () => {
-  const [showUsers, setShowUsers] = useState(true);
-
-  const toggleUsersHandler = () => {
-    setShowUsers((curState) => !curState);
-  };
-
-  const usersList = (
-    <ul>
-      {DUMMY_USERS.map((user) => (
-        <User key={user.id} name={user.name} />
-      ))}
-    </ul>
-  );
-
-  return (
-    <div className={classes.users}>
-      <button onClick={toggleUsersHandler}>
-        {showUsers ? 'Hide' : 'Show'} Users
-      </button>
-      {showUsers && usersList}
-    </div>
-  );
-};
+class Users extends Component {
+    constructor() {
+        super();
+        this.state = {
+            showUsers: true,
+            moreState: 'test',
+        }; //'the state has to be an object
+    }
+    toggleUsersHandler() {
+        this.setState((curState) => {
+            return { showUsers: !curState.showUsers };
+        }); //' the states are merges not override in class based component, which means the other states wont be erased
+    }
+    render() {
+        const usersList = (
+            <ul>
+                {DUMMY_USERS.map((user) => (
+                    <User key={user.id} name={user.name} />
+                ))}
+            </ul>
+        );
+        return (
+            <div className={classes.users}>
+                <button onClick={this.toggleUsersHandler.bind(this)}>
+                    {this.state.showUsers ? 'Hide' : 'Show'} Users
+                </button>
+                {this.state.showUsers && usersList}
+            </div>
+        );
+    }
+}
 
 export default Users;
